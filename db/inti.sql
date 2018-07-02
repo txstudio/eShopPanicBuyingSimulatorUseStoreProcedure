2b8ba66d28629a3e11c17cdec850a46e5876d1d9
Shi Hao Wang <st890409@hotmail.com>
2018-07-02 10:03:18 +0800

    建立預設資料內容的指令碼

---
+++ db/inti.sql
@@ -0,0 +1,152 @@
+/*
+--將既有 eShop 資料庫移除的指令碼
+EXEC msdb.dbo.sp_delete_database_backuphistory @database_name = N'eShop'
+GO
+
+USE [master]
+GO
+
+ALTER DATABASE [eShop]
+	SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+GO
+
+USE [master]
+GO
+
+DROP DATABASE [eShop]
+GO
+*/
+
+/*
+    建立 eShop 範例資料庫需要的資料表與預設資料內容
+*/
+CREATE DATABASE [eShop]
+GO
+
+USE [eShop]
+GO
+
+CREATE SCHEMA [Products]
+GO
+
+CREATE SCHEMA [Orders]
+GO
+
+--商品資料表
+CREATE TABLE [Products].[ProductMains]
+(
+	[No]			INT,
+	[Schema]		VARCHAR(15),
+	[Name]			NVARCHAR(50),
+	[SellPrice]		SMALLMONEY,
+	
+	CONSTRAINT [pk_Products_ProductMains] PRIMARY KEY ([No]),
+	
+	CONSTRAINT [un_Products_ProductMains_Schema] UNIQUE ([Schema])
+)
+GO
+
+INSERT INTO [Products].[ProductMains] ([No],[Schema],[Name],[SellPrice])
+VALUES 
+	(1,'DYAJ93A900930IK',N'Microsoft Surface Pro (Core i7/16G/256G/W10P)',70888)
+	,(2,'DYAJ93A900929IK',N'Microsoft Surface Pro (Core i5/8G/128G/W10P)',51888)
+	,(3,'DYAJ93A900928IK',N'Microsoft Surface Pro (Core i3/4G/128G/W10P)',41888)
+GO
+
+--商品庫存資料表
+CREATE TABLE [Products].[ProductStorages]
+(
+	[ProductNo]		INT,
+	[Storage]		SMALLINT,
+	
+	CONSTRAINT [pk_ProductStorages] PRIMARY KEY ([ProductNo]),
+	
+	CONSTRAINT [fk_ProductStorages_ProductNo] FOREIGN KEY ([ProductNo])
+		REFERENCES [Products].[ProductMains]([No]) 
+			ON DELETE NO ACTION 
+			ON UPDATE NO ACTION
+)
+GO
+
+INSERT INTO [Products].[ProductStorages] ([ProductNo],[Storage])
+	VALUES (1,15),(2,30),(3,25)
+GO
+
+--訂單資料表主索引鍵使用序列
+CREATE SEQUENCE [Orders].[OrderMainSeq]
+	START WITH 1
+	INCREMENT BY 1
+GO
+
+--訂單主資料表
+CREATE TABLE [Orders].[OrderMains]
+(
+	[No]			INT,
+	[Schema]		CHAR(15),
+	
+	[MemberGUID]	UNIQUEIDENTIFIER,
+	[IsDeleted]		BIT,
+	
+	CONSTRAINT [pk_OrderMains] PRIMARY KEY ([No]),
+	
+	CONSTRAINT [un_OrderMains_Schema] UNIQUE ([Schema])
+)
+
+--詳細訂單資料：訂購數量與單價
+CREATE TABLE [Orders].[OrderDetails]
+(
+	[OrderNo]		INT,
+	[ProductNo]		INT,
+	
+	[SellPrice]		SMALLMONEY,
+	[Quantity]		SMALLINT,
+	
+	CONSTRAINT [pk_OrderDetails] PRIMARY KEY ([OrderNo],[ProductNo]),
+	
+	CONSTRAINT [fk_OrderDetails_OrderNo] FOREIGN KEY ([OrderNo])
+		REFERENCES [Orders].[OrderMains] ([No]) 
+			ON DELETE NO ACTION 
+			ON UPDATE NO ACTION,
+			
+	CONSTRAINT [fk_OrderDetails_ProductNo] FOREIGN KEY ([ProductNo])
+		REFERENCES [Products].[ProductMains] ([No]) 
+			ON DELETE NO ACTION 
+			ON UPDATE NO ACTION
+)
+GO
+
+--取得新一筆訂單要儲存的訂單編號 (yyyyMMdd9999999)
+CREATE FUNCTION [Orders].[GetOrderSchema]()
+	RETURNS CHAR(15)
+AS
+BEGIN
+	DECLARE @Schema CHAR(15)
+	DECLARE @LastCode CHAR(8)
+	DECLARE @LastIdentity CHAR(7)
+	DECLARE @NewCode CHAR(8)
+	DECLARE @Identity INT
+
+	-- SET @Schema = (
+		-- SELECT TOP(1) [Schema] FROM [Orders].[OrderMains]
+		-- ORDER BY [No] DESC
+	-- )
+	SET @Schema = (
+		SELECT TOP(1) [Schema] 
+		FROM [Orders].[OrderMains]
+		ORDER BY [Schema] DESC
+	)
+
+	SET @NewCode = CONVERT(VARCHAR,GETDATE(),112)
+	SET @LastCode = LEFT(@Schema,8)
+	SET @LastIdentity = RIGHT(@Schema,7)
+
+	SET @Identity = 0
+
+	If @NewCode = @LastCode 
+		SET @Identity = CONVERT(INT,@LastIdentity)
+
+	SET @Identity = @Identity + 1
+
+	RETURN (@NewCode + RIGHT('000000'+CONVERT(VARCHAR(7),@Identity),7))
+END
+GO
