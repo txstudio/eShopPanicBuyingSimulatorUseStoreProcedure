c58d55c1cb16cb9bb94278c0d2cf8350f929b0df
Shi Hao Wang <st890409@hotmail.com>
2018-07-02 10:51:56 +0800

    設定 eShop 資料庫的 isolation 層級

--- db/inti.sql
+++ db/inti.sql
@@ -23,6 +23,22 @@ GO
 CREATE DATABASE [eShop]
 GO
 
+/*
+	此設定與 Azure SQL Database 相同
+	https://blogs.msdn.microsoft.com/sqlcat/2013/12/26/be-aware-of-the-difference-in-isolation-levels-if-porting-an-application-from-windows-azure-sql-db-to-sql-server-in-windows-azure-virtual-machine/
+*/
+
+--啟用 SNAPSHOT_ISOLATION
+ALTER DATABASE eShop  
+	SET ALLOW_SNAPSHOT_ISOLATION ON
+GO
+
+--啟用 READ_COMMITTED_SNAPSHOT
+ALTER DATABASE eShop  
+	SET READ_COMMITTED_SNAPSHOT ON
+	WITH ROLLBACK IMMEDIATE
+GO
+
 USE [eShop]
 GO
 
