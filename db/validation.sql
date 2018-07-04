8ddcc390cd107be6a70bbca3e3451b170b2b8fed
Shi Hao Wang <st890409@hotmail.com>
2018-07-04 10:49:13 +0800

    驗證商品購買數量是否超賣

---
+++ db/validation.sql
@@ -0,0 +1,21 @@
+/*
+    驗證商品目前庫存量與訂單總計每筆商品庫存量的查詢
+        若商品庫存量 < 訂單總計每筆商品庫存量 = 有超賣的情勢
+*/
+;WITH [OrderTable] AS (
+	SELECT b.[No]
+		,b.[Schema]
+		,SUM(a.[Quantity]) [TotalOrderQuantity]
+	FROM [Orders].[OrderDetails] a
+		INNER JOIN [Products].[ProductMains] b ON a.[ProductNo] = b.[No]
+	GROUP BY b.[No]
+		,b.[Schema]
+)
+SELECT a.[No]
+	,a.[Schema]
+	,b.[Storage]
+	,a.[TotalOrderQuantity]
+FROM [OrderTable] a
+	INNER JOIN [Products].[ProductStorages] b 
+		ON a.[No] = b.[ProductNo]
+ORDER BY a.[No] ASC
